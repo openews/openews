269f63f54e68fe2f37627876a24cdfa1beae8e8f
m-nick <sunty1@mail.ru>
2019-12-17 12:41:45 +0300

    Add piece of news

---
+++ src/components/content/VContentPieceOfNews.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="peice">
+    <h1 class="piece__title">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+    <p class="piece__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quaerat totam et tempore ex rerum sint, mollitia dolores voluptates sit! Nobis, quas facilis? Beatae a nam, sed minima perspiciatis voluptatibus quia similique cupiditate deleniti eveniet hic maiores id ab perferendis libero sequi explicabo, reprehenderit atque qui nostrum modi. Voluptas natus harum maiores nihil voluptate dignissimos, nisi consequuntur id voluptatibus, enim cumque modi sed aliquid exercitationem odio nulla veniam et rem molestias fugit corporis perferendis? A fuga voluptates, ipsum, architecto ipsam, ad nostrum necessitatibus officia saepe perspiciatis autem provident aliquid consequatur perferendis velit sunt pariatur. Fugit corrupti soluta quia officiis culpa.</p>
+    <img src="@/assets/imgs/0.jpg" alt="" class="piece__img">
+    <p class="piece__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quaerat totam et tempore ex rerum sint, mollitia dolores voluptates sit! Nobis, quas facilis? Beatae a nam, sed minima perspiciatis voluptatibus quia similique cupiditate deleniti eveniet hic maiores id ab perferendis libero sequi explicabo, reprehenderit atque qui nostrum modi. Voluptas natus harum maiores nihil voluptate dignissimos, nisi consequuntur id voluptatibus, enim cumque modi sed aliquid exercitationem odio nulla veniam et rem molestias fugit corporis perferendis? A fuga voluptates, ipsum, architecto ipsam, ad nostrum necessitatibus officia saepe perspiciatis autem provident aliquid consequatur perferendis velit sunt pariatur. Fugit corrupti soluta quia officiis culpa.</p>
+    <p class="piece__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quaerat totam et tempore ex rerum sint, mollitia dolores voluptates sit! Nobis, quas facilis? Beatae a nam, sed minima perspiciatis voluptatibus quia similique cupiditate deleniti eveniet hic maiores id ab perferendis libero sequi explicabo, reprehenderit atque qui nostrum modi. Voluptas natus harum maiores nihil voluptate dignissimos, nisi consequuntur id voluptatibus, enim cumque modi sed aliquid exercitationem odio nulla veniam et rem molestias fugit corporis perferendis? A fuga voluptates, ipsum, architecto ipsam, ad nostrum necessitatibus officia saepe perspiciatis autem provident aliquid consequatur perferendis velit sunt pariatur. Fugit corrupti soluta quia officiis culpa.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VContentPieceOfNews',
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss" src="@/styles/content/VContentPieceOfNews.scss">
+
+</style>
---
+++ src/views/PieceOfNews.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="page">
+    <Top />
+    <div class="page__time">
+      <div class="page__focus">
+        <div class="page__row">
+          <div class="page__col page__col--2">
+            <VPieceOfNews />
+          </div>
+        </div>
+      </div>
+      <div class="page__aside">
+        <div class="page__col">
+          <BlockNews header="Важные новости" :news="news3" />
+        </div>
+      </div>
+    </div>
+    <div class="page__category">
+      <div v-for="(cats, index) in categories" :key="index" class="page__row">
+        <div class="page__col">
+          <CategoryColumn :header="cats[0]" :news="categoryNews" />
+        </div>
+        <div class="page__col">
+          <CategoryColumn :header="cats[1]" :news="categoryNews" />
+        </div>
+        <div class="page__col">
+          <CategoryColumn :header="cats[2]" :news="categoryNews" />
+        </div>
+      </div>
+    </div>
+    <VFooter />
+    <button @click="scrollTop()" class="page__up"></button>
+  </div>
+</template>
+
+<script>
+import Top from '@/components/content/VContentTop'
+import VPieceOfNews from '@/components/content/VContentPieceOfNews'
+import TimeColumn from '@/components/content/VContentTimeColumn'
+import CategoryColumn from '@/components/content/VContentCategoryColumn'
+import BlockNews from '@/components/content/VContentBlockNews'
+import BigNews from '@/components/content/VContentBigNews'
+import MiddleNews from '@/components/content/VContentMiddleNews'
+import VFooter from '@/components/content/VContentFooter'
+
+export default {
+  name: 'PieceOfNews',
+  components: {
+    Top,
+    TimeColumn,
+    CategoryColumn,
+    BlockNews,
+    BigNews,
+    MiddleNews,
+    VFooter,
+    VPieceOfNews,
+  },
+  data() {
+    return {
+      shortNew: {
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat odit eveniet, debitis modi dolore quidem?',
+        path: 'example',
+        time: 1575900368369,
+        img: (i) => this.i || this.i === 0 ? require(`../assets/imgs/${this.i++}.jpg`) : require(`../assets/imgs/0.jpg`),
+      },
+      textNew: {
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat odit eveniet, debitis modi dolore quidem?',
+        path: 'example',
+      },
+      bigNew: {
+        category: 'Обо всём',
+        title: "Lorem ipsum dolor sit amet,",
+        description: "ВLorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        date: 1575900368369,
+        path: 'example',
+        img: 'https://picsum.photos/620/310',
+      },
+      configNew: {
+        title: 'Lorem ipsum',
+        description: 'Lorem ipsum dolor sit amet.',
+        date: 1575900368369,
+        path: 'example',
+        img: 'https://picsum.photos/620/310',
+        category: 'Обо всём',
+      },
+      categoryNew: {
+        img: 'https://picsum.photos/96/64',
+        title: 'Lorem ipsum',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat odit eveniet, quidem?',
+        path: 'example',
+      },
+      categories: [
+        [{ text: 'Россия', path: 'example', },
+        { text: 'Мир', path: 'example', },
+        { text: 'Бывший СССР', path: 'example', }],
+        [{ text: 'Экономика', path: 'example', },
+        { text: 'Силовые структуры', path: 'example', },
+        { text: 'Наука и техника', path: 'example', }],
+        [{ text: 'Спорт', path: 'example', },
+        { text: 'Культура', path: 'example', },
+        { text: 'интернет и СМИ', path: 'example', }],
+        [{ text: 'Ценности', path: 'example', },
+        { text: 'Путешествия', path: 'example', },
+        { text: 'Из жизни', path: 'example', }],
+        [{ text: 'Motor', path: 'example', },
+        { text: 'Дом', path: 'example', },
+        { text: 'Нацпрокеты', path: 'example', }],
+      ],
+    };
+  },
+  computed: {
+    news1() {
+      return this.generate([{ 1: this.longNew }, { 2: this.shortNew }])
+    },
+    news2() {
+      return this.generate([{ 10: this.shortNew }])
+    },
+    news3() {
+      return this.generate([{ 7: this.textNew }])
+    },
+    news4() {
+      return this.generate([{ 1: this.configNew },{ 2: this.shortNew },{ 1: this.configNew },{ 3: this.shortNew },{ 1: this.configNew },{ 4: this.shortNew },{ 1: this.configNew },{ 3: this.shortNew },{ 1: this.configNew },{ 3: this.shortNew }])
+    },
+    news5() {
+      return this.generate([{ 1: this.configNew },{ 3: this.shortNew },{ 1: this.configNew },{ 8: this.shortNew },{ 1: this.configNew },{ 3: this.shortNew },{ 1: this.configNew },{ 1: this.shortNew },{ 1: this.configNew },{ 2: this.shortNew }])
+    },
+    news6(){
+      return this.generate([{ 1: this.configNew },{ 1: this.shortNew },{ 1: this.configNew },{ 2: this.shortNew },{ 1: this.configNew },{ 1: this.shortNew }])
+    },
+    categoryNews(){
+      return this.generate([{ 1: this.textNew }, { 1: this.categoryNew }, { 1: this.textNew }, { 1: this.categoryNew }, { 1: this.textNew }, { 1: this.categoryNew }, { 1: this.textNew },])
+    }
+  },
+  methods: {
+    generate(configs){
+      this.i = 0
+      let obj = []
+      configs.forEach(config => {
+        const number = Object.keys(config)[0]
+        const example = config[number]
+        for(let i = 0; i < number; i++){
+          obj.push(example)
+        }
+      })
+      return obj
+    },
+    scrollTop(){
+      document.scrollingElement.scrollTop = 0
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss" src="@/styles/Page.scss">
+
+</style>
